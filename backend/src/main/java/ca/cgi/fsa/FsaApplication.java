507055e75c130b651b3dd420da1fd0cb5ff3c42d
Greg Pascucci <greg.pascucci@gcgi.com>
2022-05-27 14:41:41 -0700

    added spring boot files

---
+++ backend/src/main/java/ca/cgi/fsa/FsaApplication.java
@@ -0,0 +1,21 @@
+package ca.cgi.fsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FsaApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsaApplication.class, args);
+	}
+
+}
