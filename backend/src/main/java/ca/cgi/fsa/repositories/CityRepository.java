d025edd81cf62727f6299f13850249246d712bed
Greg Pascucci <greg.pascucci@gcgi.com>
2022-05-28 10:01:19 -0700

    created a test cities rest api

--- backend/src/main/java/ca/cgi/fsa/repositories/CityRepository.java
+++ backend/src/main/java/ca/cgi/fsa/repositories/CityRepository.java
@@ -1,10 +1,6 @@
 package ca.cgi.fsa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-=======
-
->>>>>>> bfd9c0c (created a test cities rest api)
 import ca.cgi.fsa.entities.City;
 
 public interface CityRepository extends JpaRepository<City, Long> {
