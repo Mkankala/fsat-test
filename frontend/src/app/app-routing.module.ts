10bc2020a2a1ce810cc916346fc89a9d84ee8d12
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2022-05-30 21:39:30 -0700

    swu-3: add simple routing.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: CurrentWeatherComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
